e3390aba64bccc9368776299a0e3efd547c4fd96
Sun <sun007021@gmail.com>
2021-10-04 14:44:01 +0900

    first

---
+++ 1/src/helloworld.java
@@ -0,0 +1,15 @@
+public class helloworld {
+    public static  void main(String[] args){
+        printHello();
+        //
+        printHello();
+        //
+    }
+
+    private static void printHello() {
+        System.out.println("____");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+
+}
